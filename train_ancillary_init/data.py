b1abb04f8ed08a2d508dafb8a2e978d9b130eec0
vanilladucky <kimhyunbin106@gmail.com>
2025-06-30 16:24:41 +0800

    fix

--- train_ancillary_init/data.py
+++ train_ancillary_init/data.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 import numpy as np
+import nibabel as nib
 
 class BreastTumor(Dataset):
     def __init__(self, info_list, transform=None):
@@ -11,13 +12,14 @@ class BreastTumor(Dataset):
 
     def __getitem__(self, idx):
         case = self.info_list[idx]
-        seg_npz = '/data/zym/YN/DCE/resample_1.0X0.6X0.6/BoxMask/' + case
-        img_npz = '/data/zym/YN/DCE/resample_1.0X0.6X0.6/DCE_C1/' + case
-        gt_npz = '/data/zym/YN/DCE/resample_1.0X0.6X0.6/TumorMask/' + case
+        seg_npz = f'/root/autodl-tmp/Kim/kits23/dataset/{case}/segmentation_pCE.nii.gz'
+        img_npz = f'/root/autodl-tmp/Kim/kits23/dataset/{case}/imaging.nii.gz' 
+        gt_npz = f'/root/autodl-tmp/Kim/kits23/dataset/{case}/segmentation.nii.gz' 
         case = case.split('.')[0]
-        seg_data = np.load(seg_npz)
-        img_data = np.load(img_npz)
-        gt_data = np.load(gt_npz)
+        seg_data = nib.load(seg_npz).get_fdata()
+        img_data = nib.load(img_npz).get_fdata()
+        gt_data = nib.load(gt_npz).get_fdata()
+        gt_data[gt_data==3] = 0 # In case of cyst
 
         img = img_data['data']
         seg = seg_data['seg']
